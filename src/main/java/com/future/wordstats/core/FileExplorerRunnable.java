ec15e2a280ca8eeed37fe6c6044bdf96a2c3ff2f
Abdelrhman Mohsen <abdulrahman_20230317@fci.helwan.edu.eg>
2025-11-19 21:47:44 +0200

    Implement FileExplorerRunnable

---
+++ src/main/java/com/future/wordstats/core/FileExplorerRunnable.java
@@ -0,0 +1,66 @@
+package com.future.wordstats.core;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class FileExplorerRunnable implements Runnable {
+
+    private final ConcurrentLinkedQueue<FileResult> fileResultsQueue;
+    private final AtomicReference<String> longestWord;
+    private final AtomicReference<String> shortestWord;
+    private final AtomicBoolean stopped;
+
+    public FileExplorerRunnable(ConcurrentLinkedQueue<FileResult> fileResultsQueue,
+                                AtomicReference<String> longestWord,
+                                AtomicReference<String> shortestWord,
+                                AtomicBoolean stopped) {
+        this.fileResultsQueue = fileResultsQueue;
+        this.longestWord = longestWord;
+        this.shortestWord = shortestWord;
+        this.stopped = stopped;
+    }
+
+    @Override
+    public void run() {
+        FileResult fileResult;
+        while (!stopped.get() && (fileResult = fileResultsQueue.poll()) != null) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(fileResult.getFilePath()))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    updateFileResult(fileResult, line);
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private void updateFileResult(FileResult fileResult, String line) {
+        String[] words = line.split(" ");
+        for (String word : words) {
+            fileResult.incrementNumOfWords();
+            switch (word) {
+                case "is" -> fileResult.incrementNumOfIsWords();
+                case "are" -> fileResult.incrementNumOfAreWords();
+                case "you" -> fileResult.incrementNumOfYouWords();
+            }
+
+            if (word.trim().isEmpty()) {
+                return;
+            }
+
+            fileResult.compareAndSetIfLonger(word);
+            fileResult.compareAndSetIfShorter(word);
+
+            longestWord.updateAndGet(current -> word.length() > current.length() ? word : current);
+            shortestWord.updateAndGet(current -> {
+                if (current.isEmpty()) return word;
+                return word.length() < current.length() ? word : current;
+            });
+        }
+    }
+
+}
